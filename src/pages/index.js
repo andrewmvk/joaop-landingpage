bcc4be8ebc5738c77ffc217befd3ae83d745c549
andrewmvk <andrew.medeiros00@gmail.com>
2023-10-11 22:17:50 -0300

    Last changes and working contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,49 +11,52 @@ import ContactSection from "@/components/ContactSection";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>João Paulo</title>
-        <meta name="description" content="Landing Page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={inter.className}>
-        <div className={styles.headerCon}>
-          <Image
-            src="/logo.png"
-            alt="Logo: O Direito de Viver Bem"
-            className={styles.logo}
-            width={100}
-            height={100}
-            quality={100}
-          />
-          <Header />
-          <div className={styles.mainImgCon}>
-            <Image
-              className={styles.mainImage}
-              src="/main-image.png"
-              alt="Advogado Joao Paulo"
-              quality={100}
-              priority
-              fill
+   return (
+      <>
+         <Head>
+            <title>João Paulo</title>
+            <meta name="description" content="Landing Page" />
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1"
             />
-          </div>
-          <div className={styles.mainDetails}>
-            <h1>João Paulo Borges</h1>
-            <h2>
-              Especialista em <span>DIREITO MÉDICO</span> e da{" "}
-              <span>SAÚDE</span>
-            </h2>
-            <button className="button-container">Entre em contato</button>
-          </div>
-        </div>
-        <LawyerReference />
-        <AboutSection />
-        <OcuppationSection />
-        <ContactSection />
-      </main>
-    </>
-  );
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <main className={inter.className}>
+            <div className={styles.headerCon}>
+               <Image
+                  src="/logo.png"
+                  alt="Logo: O Direito de Viver Bem"
+                  className={styles.logo}
+                  width={100}
+                  height={100}
+                  quality={100}
+               />
+               <Header />
+               <div className={styles.mainImgCon}>
+                  <Image
+                     className={styles.mainImage}
+                     src="/main-image.png"
+                     alt="Advogado Joao Paulo"
+                     quality={100}
+                     priority
+                     fill
+                  />
+               </div>
+               <div className={styles.mainDetails}>
+                  <h1>João Paulo Borges</h1>
+                  <h2>
+                     Especialista em <span>DIREITO MÉDICO</span> e da{" "}
+                     <span>SAÚDE</span>
+                  </h2>
+                  <button className="button-container">Entre em contato</button>
+               </div>
+            </div>
+            <LawyerReference />
+            <AboutSection />
+            <OcuppationSection />
+            <ContactSection />
+         </main>
+      </>
+   );
 }
